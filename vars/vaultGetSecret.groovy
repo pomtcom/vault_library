9e37b2ac3913b7023352ead34249f36378a77c11
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-24 10:53:31 +0700

    test param from main pipeline

--- vars/vaultGetSecret.groovy
+++ vars/vaultGetSecret.groovy
@@ -1,4 +1,8 @@
-// groovy library for Vault get secret
+import groovy.json.JsonSlurper
+
+// def role_id = params.role_id
+// def secret_id
+// def role_token
 
 def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
@@ -9,9 +13,10 @@ def call(String name = 'human') {
 
     node {
 
-        stage('TEST node') {
+        stage('TEST params node') {
             print 'test node from library';
             // print 'role id is ' + role_id
+            print 'role_id is ' + params.role_id
         }
 
     }
