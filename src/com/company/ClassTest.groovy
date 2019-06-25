41738370c3840f544353ae0b30847c14f4717b71
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-25 17:18:43 +0700

    test object

--- src/com/company/ClassTest.groovy
+++ src/com/company/ClassTest.groovy
@@ -1,8 +1,14 @@
 class ClassTest {
-    String testString
+    String testString;
     def method1(){
         print 'hello from method 1';
-        echo 'HHHHHHHHHHHHH';
-        return "99999999" ;
+        // echo 'HHHHHHHHHHHHH';
+        // return "99999999" ;
+        if (testString == 3){
+            echo "THIS IS WORKING" ;
+        }else{
+            echo "THIS IS NOT WORKING" ;
+        }
+
     }
 }
