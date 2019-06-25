e2d4d7872e7e37df5b3f4f2b664df7f67d06e505
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-25 17:30:53 +0700

    test object

--- src/com/company/ClassTest.groovy
+++ src/com/company/ClassTest.groovy
@@ -1,14 +1,25 @@
 class ClassTest {
+
+    Script script;
+
+    ClassTest(script) {          
+        this.script = script;
+    }
+
+
     String testString;
     def testInt
+
     def method1(){
         print 'hello from method 1';
         // echo 'HHHHHHHHHHHHH';
         // return "99999999" ;
         if (testInt == 3){
-            echo "THIS IS WORKING" ;
+            // echo "THIS IS WORKING" ;
+            script.echo("this is working");
         }else{
-            echo "THIS IS NOT WORKING" ;
+            // echo "THIS IS NOT WORKING" ;
+            script.echo("this is not working");
         }
 
     }
