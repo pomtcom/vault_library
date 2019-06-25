bc5546aa5e21749fa3b627874b75fb1b5592e58f
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-25 17:45:27 +0700

    building stage

--- src/com/company/ClassTest.groovy
+++ src/com/company/ClassTest.groovy
@@ -14,6 +14,18 @@ class ClassTest {
         print 'hello from method 1';
         // echo 'HHHHHHHHHHHHH';
         // return "99999999" ;
+
+        node {
+            stage('XXXXXX'){
+                script.echo("this is stage from pipeline XX");
+            }
+            stage('YYYYYY'){
+                script.echo("This is stage from pipeline YY");
+            }
+        }
+
+
+
         if (testInt == 3){
             // echo "THIS IS WORKING" ;
             script.echo("this is working");
