6411dee5703924b029fcda4d8b0a5c072d176c96
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-25 17:20:54 +0700

    change from string to int

--- src/com/company/ClassTest.groovy
+++ src/com/company/ClassTest.groovy
@@ -1,10 +1,11 @@
 class ClassTest {
     String testString;
+    def testInt
     def method1(){
         print 'hello from method 1';
         // echo 'HHHHHHHHHHHHH';
         // return "99999999" ;
-        if (testString == 3){
+        if (testInt == 3){
             echo "THIS IS WORKING" ;
         }else{
             echo "THIS IS NOT WORKING" ;
