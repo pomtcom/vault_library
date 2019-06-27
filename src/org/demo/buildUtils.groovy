2978a433625a58700e5df3da0d090ea9ebd5796d
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-27 16:34:26 +0700

    rename package

--- src/org/demo/buildUtils.groovy
+++
@@ -1,9 +0,0 @@
-package org.demo
-
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
-}
-
-def echoHelloWorld(){
-    print 'THIS IS ECHO HELLO WOLRD from object library' ;
-}
