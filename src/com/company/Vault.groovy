e0b50034c53c2ff3d3e043c8404449cd61f2889c
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-27 15:33:43 +0700

    test get secret

--- src/com/company/Vault.groovy
+++ src/com/company/Vault.groovy
@@ -59,46 +59,47 @@ class Vault {
 
         }
 
-        // script.stage('Vault - generate role_token'){
-        //     print 'generating role_token'
-        //     def post = new URL(vaultHostAddr + "/v1/auth/approle/login").openConnection();
-        //     def message = '{"role_id": "' + role_id + '",' + '"secret_id": "' + secret_id + '"}';
-        //     post.setRequestMethod("POST");
-        //     post.setDoOutput(true);
-        //     post.getOutputStream().write(message.getBytes("UTF-8"));
-        //     if(post.getResponseCode().equals(200)) {
-        //         def jsonResponse = post.getInputStream().getText() ;
-        //         def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
-        //         role_token = jsonSlurped['auth']['client_token'];
-        //         // print('role_token is ' + role_token);
-        //     }
-        //     else{
-        //         error("error for calling " + vaultHostAddr + "/v1/auth/approle/login");
-        //         println('http error response code ' + post.getResponseCode());
-        //     }
+        script.stage('Vault - generate role_token'){
+            // print 'generating role_token'
+            def post = new URL(vaultHostAddr + "/v1/auth/approle/login").openConnection();
+            def message = '{"role_id": "' + role_id + '",' + '"secret_id": "' + secret_id + '"}';
+            post.setRequestMethod("POST");
+            post.setDoOutput(true);
+            post.getOutputStream().write(message.getBytes("UTF-8"));
+            if(post.getResponseCode().equals(200)) {
+                def jsonResponse = post.getInputStream().getText() ;
+                def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
+                role_token = jsonSlurped['auth']['client_token'];
+                // print('role_token is ' + role_token);
+            }
+            else{
+                error("error for calling " + vaultHostAddr + "/v1/auth/approle/login");
+                println('http error response code ' + post.getResponseCode());
+            }
 
-        //     assert role_token != null : 'role_token is not generated, please check role_id and secret_id for AppRole' ;
+            assert role_token != null : 'role_token is not generated, please check role_id and secret_id for AppRole' ;
 
         // // print('message to send is ' + message);
-        // }
-        // script.stage('Vault - get secret'){
-        //     print 'getting secrt'
-        //     def get = new URL(vaultHostAddr + "/v1/secret_poc/vault_poc_path").openConnection();
-        //     get.setRequestProperty("X-Vault-Token", role_token)
-        //     def getRC = get.getResponseCode();
-        //     if(getRC.equals(200)) {
-        //         def jsonResponse = get.getInputStream().getText() ;
-        //         def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
-                
-        //         def poc_password = jsonSlurped['data']['MySQL_PASSWORD'];
-        //         print('MySQL_PASSWORD is ' + poc_password) ;
-        //     }
-        //     else{
-        //         error("error for calling " + vaultHostAddr + "/v1/secret_poc/vault_poc_path");
-        //         println('http error response code ' + getRC);
-        //     }
+        }
 
-        // }
+        script.stage('Vault - get secret'){
+            // print 'getting secrt'
+            def get = new URL(vaultHostAddr + "/v1/secret_poc/vault_poc_path").openConnection();
+            get.setRequestProperty("X-Vault-Token", role_token)
+            def getRC = get.getResponseCode();
+            if(getRC.equals(200)) {
+                def jsonResponse = get.getInputStream().getText() ;
+                def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
+                
+                def poc_password = jsonSlurped['data']['MySQL_PASSWORD'];
+                print('MySQL_PASSWORD is ' + poc_password) ;
+            }
+            else{
+                error("error for calling " + vaultHostAddr + "/v1/secret_poc/vault_poc_path");
+                println('http error response code ' + getRC);
+            }
+
+        }
     }
 
 
