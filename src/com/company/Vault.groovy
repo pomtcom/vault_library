e6099c9c7e49e6bc48f108110d68a934aefb9ae5
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-27 16:21:44 +0700

    clean code

--- src/com/company/Vault.groovy
+++ src/com/company/Vault.groovy
@@ -32,22 +32,16 @@ class Vault {
         script.stage('Vault - create secret_id'){
             
             script.withCredentials([script.string(credentialsId: 'VaultToken', variable: 'vaultToken')]) {
-                // echo "My password is '${vaultToken}'!"
-
                 def post = new URL(vaultHostAddr + "/v1/auth/approle/role/vault_poc_role/secret-id").openConnection();
                 def message = '{}'
                 post.setRequestMethod("POST")
                 post.setDoOutput(true)
-
                 post.setRequestProperty("X-Vault-Token", script.vaultToken)
                 post.getOutputStream().write(message.getBytes("UTF-8"));
-                // println(postRC);
                 if(post.getResponseCode().equals(200)) {
                     def jsonResponse = post.getInputStream().getText() ;
                     def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
                     secret_id = jsonSlurped['data']['secret_id'];
-                    // print('secret_id is ' + secret_id)
-
                 }
                 else{
                     error("error for calling " + vaultHostAddr + "/v1/auth/approle/role/vault_poc_role/secret-id");
@@ -60,7 +54,7 @@ class Vault {
         }
 
         script.stage('Vault - generate role_token'){
-            // print 'generating role_token'
+            script.echo('generate role_token');
             def post = new URL(vaultHostAddr + "/v1/auth/approle/login").openConnection();
             def message = '{"role_id": "' + role_id + '",' + '"secret_id": "' + secret_id + '"}';
             post.setRequestMethod("POST");
@@ -70,7 +64,6 @@ class Vault {
                 def jsonResponse = post.getInputStream().getText() ;
                 def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
                 role_token = jsonSlurped['auth']['client_token'];
-                // print('role_token is ' + role_token);
             }
             else{
                 error("error for calling " + vaultHostAddr + "/v1/auth/approle/login");
@@ -78,94 +71,24 @@ class Vault {
             }
 
             assert role_token != null : 'role_token is not generated, please check role_id and secret_id for AppRole' ;
-
-        // // print('message to send is ' + message);
         }
-
-        // script.stage('Vault - get secret'){
-        //     // print 'getting secrt'
-        //     def get = new URL(vaultHostAddr + "/v1/secret_poc/vault_poc_path").openConnection();
-        //     get.setRequestProperty("X-Vault-Token", role_token)
-        //     def getRC = get.getResponseCode();
-        //     if(getRC.equals(200)) {
-        //         def jsonResponse = get.getInputStream().getText() ;
-        //         def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
-                
-        //         def poc_password = jsonSlurped['data']['MySQL_PASSWORD'];
-        //         script.echo('MySQL_PASSWORD is ' + poc_password) ;
-        //     }
-        //     else{
-        //         error("error for calling " + vaultHostAddr + "/v1/secret_poc/vault_poc_path");
-        //         script.echo('http error response code ' + getRC);
-        //     }
-
-        // }
     }
 
-    // def test_return() {
-    //     script.echo('test return is executing');
-    //     return 999999999;
-    // }
-
     def getSecret(path, secret_key){
-
-            // def get = new URL(vaultHostAddr + "/v1/secret_poc/vault_poc_path").openConnection();
             def get = new URL(vaultHostAddr + "/v1/" + path).openConnection();
             get.setRequestProperty("X-Vault-Token", role_token)
             def getRC = get.getResponseCode();
             if(getRC.equals(200)) {
                 def jsonResponse = get.getInputStream().getText() ;
                 def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
-                
-                // def poc_password = jsonSlurped['data']['MySQL_PASSWORD'];
-
-                script.echo('url is ' + vaultHostAddr + "/v1/" + path);
-                script.echo('role_token is ' + role_token) ;
-
-
                 def secretValue = jsonSlurped['data'][secret_key];
-                script.echo('secretKey is ' + secretValue) ;
-
                 assert secretValue != null : 'cannot get secretValue from path ' +  "/v1/" + path + ' with key ' + secret_key;
-
                 return secretValue ;
             }
             else{
                 error("error for calling " + vaultHostAddr + "/v1/secret_poc/vault_poc_path");
                 script.echo('http error response code ' + getRC);
             }
-
-
-
-    }
-
-
-
-    String testString;
-    def testInt
-
-    def method1(){
-        print 'hello from method 1';
-        // echo 'HHHHHHHHHHHHH';
-        // return "99999999" ;
-
-        if (testInt == 3){
-            // echo "THIS IS WORKING" ;
-            script.echo("this is working");
-            script.echo("environment param is ");
-            // script.echo(params.environment);
-            // script.echo('test again');
-            script.echo(script.params.environment);
-            script.echo('end of test');
-
-            script.stage("NEW STAGE XXXXXXX"){
-                script.echo('NEW STAGE XXXXXXX');
-            }
-
-        }else{
-            // echo "THIS IS NOT WORKING" ;
-            script.echo("this is not working");
-        }
 
     }
 }
