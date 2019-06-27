98bdb1e764a54eea9628d7bb9f962a2d094ce483
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-27 16:30:03 +0700

    update assert state

--- src/com/company/Vault.groovy
+++ src/com/company/Vault.groovy
@@ -69,23 +69,25 @@ class Vault {
         }
         assert role_token != null : 'role_token is not generated, please check role_id and secret_id for AppRole' ;
     }
-    
+
 
     def getSecret(path, secret_key){
-            def get = new URL(vaultHostAddr + "/v1/" + path).openConnection();
-            get.setRequestProperty("X-Vault-Token", role_token)
-            def getRC = get.getResponseCode();
-            if(getRC.equals(200)) {
-                def jsonResponse = get.getInputStream().getText() ;
-                def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
-                def secretValue = jsonSlurped['data'][secret_key];
-                assert secretValue != null : 'cannot get secretValue from path ' +  "/v1/" + path + ' with key ' + secret_key;
-                return secretValue ;
-            }
-            else{
-                error("error for calling " + vaultHostAddr + "/v1/secret_poc/vault_poc_path");
-                script.echo('http error response code ' + getRC);
-            }
+
+        assert vaultHostAddr != null && role_token != null: 'vaultHostAddr or role_token are null value, please check that you have call .init() before getSecret' ;
+        def get = new URL(vaultHostAddr + "/v1/" + path).openConnection();
+        get.setRequestProperty("X-Vault-Token", role_token)
+        def getRC = get.getResponseCode();
+        if(getRC.equals(200)) {
+            def jsonResponse = get.getInputStream().getText() ;
+            def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
+            def secretValue = jsonSlurped['data'][secret_key];
+            assert secretValue != null : 'cannot get secretValue from path ' +  "/v1/" + path + ' with key ' + secret_key;
+            return secretValue ;
+        }
+        else{
+            error("error for calling " + vaultHostAddr + "/v1/secret_poc/vault_poc_path");
+            script.echo('http error response code ' + getRC);
+        }
 
     }
 }
