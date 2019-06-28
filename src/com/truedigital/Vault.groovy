5b10592a4f4755ea67ba6db8b239cc9d3a7a8db4
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-06-28 15:00:33 +0700

    update library to use full path

--- src/com/truedigital/Vault.groovy
+++ src/com/truedigital/Vault.groovy
@@ -7,6 +7,7 @@ class Vault {
     def secret_id
     def role_token
     def microservice_name
+    def environment
     def vaultHostAddr
 
 
@@ -17,19 +18,19 @@ class Vault {
     def init(){
         
         // ============================ Getting environment params ===============================
+        assert script.env.JOB_BASE_NAME != null : 'Get pipeline name has problem, please check'
         microservice_name = script.env.JOB_BASE_NAME;
-        script.echo('microservice_name is ' + microservice_name);
 
         assert script.params.role_id != null : 'String param role_id should be assigned' ;
-        role_id = script.params.role_id
+        role_id = script.params.role_id ;
 
         assert script.params.environment != null : 'String param environment should be assigned' ;
-        if (script.params.environment.toLowerCase() == 'dev' || 
-        script.params.environment.toLowerCase() == 'alpha' || 
-        script.params.environment.toLowerCase() == 'preprod' ) {
+        environment = script.params.environment.toLowerCase() ;
+
+        if (environment == 'dev' || environment == 'alpha' || environment == 'preprod' ) {
             vaultHostAddr = 'http://10.198.105.221:8200'
         }
-        else if (script.params.environment.toLowerCase() == 'prod'){
+        else if (environment == 'prod'){
             // TBD HostAddress for production Vault Cluster
             vaultHostAddr = ''
         }
@@ -74,21 +75,23 @@ class Vault {
     }
 
 
-    def getSecret(path, secret_key){
+    def getSecret(category, secret_key){
 
         assert vaultHostAddr != null && role_token != null: 'vaultHostAddr or role_token are null value, please check that you have call .init() before getSecret' ;
-        def get = new URL(vaultHostAddr + "/v1/" + path).openConnection();
+        def secret_path = microservice_name + '/' + environment + '/' + category ;
+        script.echo('secret_path is ' + secret_path) ;
+        def get = new URL(vaultHostAddr + "/v1/" + secret_path).openConnection();
         get.setRequestProperty("X-Vault-Token", role_token)
         def getRC = get.getResponseCode();
         if(getRC.equals(200)) {
             def jsonResponse = get.getInputStream().getText() ;
             def jsonSlurped = new JsonSlurper().parseText(jsonResponse);
             def secretValue = jsonSlurped['data'][secret_key];
-            assert secretValue != null : 'cannot get secretValue from path ' +  "/v1/" + path + ' with key ' + secret_key;
+            assert secretValue != null : 'cannot get secretValue from secret_path ' +  "/v1/" + secret_path + ' with key ' + secret_key;
             return secretValue ;
         }
         else{
-            error("error for calling " + vaultHostAddr + "/v1/" + path);
+            error("error for calling " + vaultHostAddr + "/v1/" + secret_path);
             script.echo('http error response code ' + getRC);
         }
 
